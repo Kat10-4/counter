2520d37db97ae56dfd11e4e056ea0062eb7089b9
Kat10-4 <143129834+Kat10-4@users.noreply.github.com>
2024-12-20 18:13:53 +0100

    add counter-reducer.ts

--- src/model/counter-reducer.ts
+++ src/model/counter-reducer.ts
@@ -1,10 +1,30 @@
-export function counterReducer(state, action) {
+export type ValuesTypes = {
+    max: number;
+    start: number;
+};
+
+type CounterTypes = {
+    values: { max: number, start: number },
+    counter: number,
+    isActive: boolean,
+    error: boolean,
+}
+
+const initialState: CounterTypes = {
+    values: {max: 5, start: 0},
+    counter: 0,
+    isActive: false,
+    error: false,
+};
+
+
+export function counterReducer(state: CounterTypes = initialState, action: ActionsType) {
     switch (action.type) {
         case 'SET_VALUES': {
-            const { max, start } = action.payload;
+            const {max, start} = action.payload;
             return {
                 ...state,
-                values: { max, start },
+                values: {max, start},
                 counter: start,
                 error: start < 0 || start > max, // Error validation
             };
@@ -33,3 +53,37 @@ export function counterReducer(state, action) {
             return state;
     }
 }
+
+
+// Action creators
+
+export const setValues = (values: ValuesTypes) => {
+    return {type: 'SET_VALUES', payload: values} as const
+};
+export const increment = () => {
+    return {type: 'INCREMENT'} as const
+};
+export const reset = () => {
+    return {type: 'RESET'} as const
+};
+export const toggleActive = (isActive: boolean) => {
+    return {type: 'TOGGLE_ACTIVE', payload: isActive} as const
+};
+export const setError = (error: boolean) => {
+    return {type: 'SET_ERROR', payload: error} as const};
+
+
+// Actions types
+    export type SetValuesActionType = ReturnType<typeof setValues>
+    export type IncrementActionType = ReturnType<typeof increment>
+    export type ResetActionType = ReturnType<typeof reset>
+    export type ToggleActiveActionType = ReturnType<typeof toggleActive>
+    export type SetErrorActionType = ReturnType<typeof setError>
+
+
+    type ActionsType =
+        SetValuesActionType
+        | IncrementActionType
+        | ResetActionType
+        | ToggleActiveActionType
+        | SetErrorActionType
